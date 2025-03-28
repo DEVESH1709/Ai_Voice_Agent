cb15d9694a216436c01f612ac30245ed3c951087
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-03-28 12:34:47 +0530

    Fetch user-specific interviews and display them in the Home page; add functions to retrieve interviews by user ID and latest interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,19 @@ import Image from "next/image";
 import Link from "next/link";
 import {dummyInterviews} from "@/constants"
 import InterviewCard from "@/components/InterviewCard";
-export default function Home() {
+import {getCurrentUser, getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/auth.action";
+const Page=async()=> {
+
+  const user =await getCurrentUser();
+const [userInterviews, latestInterviews]= await Promise.all([
+  await getInterviewsByUserId(user?.id!),
+  await getLatestInterviews({userId:user?.id!}),
+])
+
+ 
+
+  const hasPastIntervies= userInterviews.length > 0;
+ const hasUpcomingInterviews =latestInterviews?.length>0;
   return (
    <>
    <section className="card-cta">
@@ -23,10 +35,18 @@ export default function Home() {
    <section className="flex flex-col gap-6 mt-8">
     <h2>Your Interviews</h2>
     <div className="interviews-section">
-    {dummyInterviews.map((interview) => (
-  <InterviewCard {...interview} key={interview.id} />
-))}
+    {
 
+      hasPastIntervies?(
+        userInetrviews?.map((interview)=>(
+          <InterviewCard {...interview} key={interview.id} />
+        ))
+      ):(
+        <p>You havn&apos;t taken any interviews yet</p>
+      )
+    
+    
+    }
     </div>
 
    </section>
@@ -34,10 +54,18 @@ export default function Home() {
    <section className="flex flex-col gap-6 mt-8" >
     <h2>Take an Interviews</h2>
     <div className="inerviews-section">
-    {dummyInterviews.map((interview) => (
-  <InterviewCard {...interview} key={interview.id} />
-))}
+    {
 
+hasUpcomingInterveiws?(
+  latestInterviews?.map((interview)=>(
+    <InterviewCard {...interview} key={interview.id} />
+  ))
+):(
+  <p>There are no new interviews available </p>
+)
+
+ 
+}
     </div>
    </section>
    </>
